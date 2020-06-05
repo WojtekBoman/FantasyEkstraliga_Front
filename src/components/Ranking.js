af43a0d8e71f22bf7bfb20e2ea88e9061868e17b
WojtekBoman <wojciechboman@gmail.com>
2020-06-05 17:36:30 +0200

    Add form to Ranking

--- src/components/Ranking.js
+++ src/components/Ranking.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import authHeader from '../services/auth-header'
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCrown } from '@fortawesome/free-solid-svg-icons'
@@ -11,10 +14,16 @@ class Ranking extends React.Component {
     constructor(props) {
         super(props);
 
+        this.handleSearch = this.handleSearch.bind(this);
+        this.onChangeSearchData = this.onChangeSearchData.bind(this);
+        this.onChangeTypeSearch = this.onChangeTypeSearch.bind(this);
+
         this.state = {
             loading:false,
             message:'',
-            users:[]
+            users:[],
+            searchData:'',
+            typeSearch:'team'
         }
     }
 
@@ -30,6 +39,20 @@ class Ranking extends React.Component {
           fetch(url,options).then(res => res.json()).then(res => this.setState({users:res,loading:false}));
     }
 
+    onChangeSearchData(e) {
+        this.setState({searchData:e.target.value})
+    }
+
+    onChangeTypeSearch(e) {
+        this.setState({typeSearch:e.target.value})
+    }
+
+    handleSearch(e) {
+        e.preventDefault();
+
+        console.log(this.state);
+    }
+
     render() {
 
         console.log(this.state.users)
@@ -37,10 +60,49 @@ class Ranking extends React.Component {
         return(
             <div className="container bg-light border rounded border-dark" id="ranking">
                 <header>
-                    <h3>Ranking graczy</h3>
+                    <h1>Ranking graczy</h1>
                     <hr className="my-4"></hr>
                 </header>
+                <Form onSubmit={this.handleSearch} ref={c => this.form = c}>
+                    <header>
+                    <h4>Wyszukaj zespół</h4>
+                    <hr className="my-4"></hr>
+                    </header>
+                    <div class="form-group">
+                        <label for="exampleFormControlSelect1">Opcje wyszukiwania</label>
+                        <select onChange={this.onChangeTypeSearch} value={this.state.typeSearch} class="form-control" id="exampleFormControlSelect1">
+                        <option value="user">Nazwa gracza</option>
+                        <option value="team">Nazwa drużyny</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Wprowadź dane</label>
+                        <Input onChange={this.onChangeSearchData} value={this.state.onChangeSearchData} type="text" class="form-control" id="exampleFormControlInput1" placeholder="Wprowadź dane"/>
+                    </div>
+                    <button
+                className="btn btn-block btn-dark"
+                disabled={this.state.loading}
+              >
+                {this.state.loading && (
+                  <span className="spinner-border spinner-border-sm"></span>
+                )}
+                <span>Szukaj</span>
+              </button>
 
+                {this.state.message && (
+                  <div className="form-group">
+                    <div className="alert alert-danger" role="alert">
+                      {this.state.message}
+                    </div>
+                  </div>
+                )}
+                <CheckButton
+                  style={{display:"none"}}
+                  ref={c => {this.checkBtn = c;}}
+                  />
+                </Form>
+                <hr className="my-4"></hr>
+                
                 {!this.state.loading ? (
                     <table className="table table-striped">
                     <thead className="thead-dark">
