b832deeedf9787013111d1c6e2549ecd15f671d0
WojtekBoman <wojciechboman@gmail.com>
2020-06-06 19:40:32 +0200

    Fix buttons on slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,6 +6,8 @@ import {Carousel,Button} from 'react-bootstrap';
 import Background1 from '../img/Background1.jpg'
 import Background2 from '../img/Background2.jpg'
 import Background3 from '../img/Background3.jpg'
+import { HashLink } from 'react-router-hash-link';
+import {Link} from "react-router-dom";
 
 class Slider extends React.Component {
 
@@ -33,8 +35,8 @@ class Slider extends React.Component {
       </div>) 
       : (<div> <p class="titleOfScreen">Załóż drużynę już dzisiaj</p>
       <h3>Załóż bezpłatnie konto</h3>
-      <Button variant="warning" className="buttons">Rejestracja</Button>
-      <Button variant="light" className="buttons">Logowanie</Button></div>)}
+      <Link to="/logowanie"><Button variant="warning" className="buttons">Rejestracja</Button></Link>
+      <Link to="/logowanie"><Button variant="light" className="buttons">Logowanie</Button></Link></div>)}
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
@@ -46,7 +48,7 @@ class Slider extends React.Component {
 
     <Carousel.Caption>
     <p class="titleOfScreen">Poznaj zasady gry !</p>
-    <a href="#info"><Button variant="outline-light">Zasady</Button></a>
+    <HashLink to="#info"><Button variant="outline-light">Zasady</Button></HashLink>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
@@ -59,7 +61,7 @@ class Slider extends React.Component {
     <Carousel.Caption>
     <p class="titleOfScreen">Kontakt nigdy nie był tak prosty</p>
       <p>Wybierz dogodną opcję kontaku</p>
-      <a href="#footer"><Button variant="outline-light">Kontakt</Button></a>
+      <HashLink to="#footer"><Button variant="outline-light">Kontakt</Button></HashLink>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
