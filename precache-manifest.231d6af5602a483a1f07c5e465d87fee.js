d9917c02d13f99ef6ab643fca3db962cded2889c
WojtekBoman <wojciechboman@gmail.com>
2020-06-06 18:55:46 +0200

    Updates

---
+++ precache-manifest.231d6af5602a483a1f07c5e465d87fee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b9cd95ff006dce7e44101d56b540cd",
+    "url": "/FantasyEkstraliga_Front/index.html"
+  },
+  {
+    "revision": "79fe42c3525840284758",
+    "url": "/FantasyEkstraliga_Front/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c14ffc32baed184790ec",
+    "url": "/FantasyEkstraliga_Front/static/css/main.ad5c1782.chunk.css"
+  },
+  {
+    "revision": "79fe42c3525840284758",
+    "url": "/FantasyEkstraliga_Front/static/js/2.a26cd9bc.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/FantasyEkstraliga_Front/static/js/2.a26cd9bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c14ffc32baed184790ec",
+    "url": "/FantasyEkstraliga_Front/static/js/main.d716fb67.chunk.js"
+  },
+  {
+    "revision": "9074d6194f55f0bc4f89",
+    "url": "/FantasyEkstraliga_Front/static/js/runtime-main.3ea28cf2.js"
+  },
+  {
+    "revision": "cbc94b73daca40c1a12e98134230b85b",
+    "url": "/FantasyEkstraliga_Front/static/media/Background1.cbc94b73.jpg"
+  },
+  {
+    "revision": "2afe50e2ee8618477977de9dd90b2a23",
+    "url": "/FantasyEkstraliga_Front/static/media/Background2.2afe50e2.jpg"
+  },
+  {
+    "revision": "fd37dffe5a2064e48b5cddc7ee1b774f",
+    "url": "/FantasyEkstraliga_Front/static/media/Background3.fd37dffe.jpg"
+  }
+]);
